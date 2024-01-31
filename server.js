f68785146953b8d59e25f2ad422971d4b3bd4e83
Ryanwalker2 <148826569+Ryanwalker2@users.noreply.github.com>
2024-01-31 18:29:05 -0500

    Setup repo and directory layouts

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+const { clog } = require('./middleware/clog.js');
+const api = require('./public/assets/js/index.js');
+
+const PORT = 3001;
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use('/api', api);
